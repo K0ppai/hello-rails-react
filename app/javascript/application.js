9f2e7cb237cfa369fc4c5e9d1e1f10f0f9b9e40d
K0ppai <psthu.koppai@gmail.com>
2023-10-23 20:31:20 +0630

    Create require components

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,11 +1,20 @@
 // Entry point for the build script in your package.json
-import "@hotwired/turbo-rails"
-import "./controllers"
+import '@hotwired/turbo-rails';
+import './controllers';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './src/App';
+import { Provider } from 'react-redux';
+import store from './src/redux/store';
 
-function App() {
-  return <h1>Hello!</h1>;
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+);
